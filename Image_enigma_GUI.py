5d8141648e371f1192ab0661209aa9c76a6ee6c1
Om Rawal <omrawal007@gmail.com>
2021-01-08 18:48:46 +0530

    Backend linked to frontend for encryption

--- Image_enigma_GUI.py
+++ Image_enigma_GUI.py
@@ -1,8 +1,26 @@
 import tkinter as tk
 from tkinter.filedialog import askopenfilename
-def getInputs(ent1_val):
-    print(ent1_val)
-    print(type(ent1_val))
+from tkinter import messagebox
+from steganography import Steganography
+import time
+import cv2
+
+
+
+def encrypt_image(message,mask,root):
+    steg_var=Steganography()
+    mask_img=cv2.imread(mask)
+    secret_message_img=cv2.imread(message)
+    cypher=steg_var.embed_a_in_b(secret_message_img,mask_img)
+    filename='./encrypted_images/'+time.strftime("%Y%m%d-%H%M%S")+'.jpg'
+    cv2.imwrite(filename, cypher)
+    result_str='File saved at \"{}\"'.format(filename)
+    messagebox.showinfo('SUCCESS!!!',result_str)
+    cv2.imshow('cypher image',cypher)
+    cv2.waitKey()
+
+def decrypt_image(hidden,root):
+    
     pass
 
 root=tk.Tk()    
@@ -39,7 +57,7 @@ mask_entry.place(x=150, y=150)
 mask_browse_button=tk.Button(root,text="Browse",font=10,command=lambda:browsefunc(mask_entry))
 mask_browse_button.place(x=410, y=140)
 
-log=tk.Button(root,font=("times", 16),text="Encrypt",command=lambda:getInputs(message_entry.get()))
+log=tk.Button(root,font=("times", 16),text="Encrypt",command=lambda:encrypt_image(message_entry.get(),mask_entry.get(),root))
 log.place(x = 200, y = 200 )
 
 root.mainloop()
